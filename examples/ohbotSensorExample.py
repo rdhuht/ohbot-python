5870f1ef454536b031208d2da1a1f9a5a9c101b4
rdhuht <1083376784@qq.com>
2020-07-16 14:48:34 +0800

    Update ohbotSensorExample.py

--- examples/ohbotSensorExample.py
+++ examples/ohbotSensorExample.py
@@ -1,24 +1,28 @@
 from ohbot import ohbot
-
+from time import *
 ## Example program for using sensors with ohbot.
 ## Tilt sensor - a3
 ## Light sensor - a4
- 
+## Touch sensor - a5
+
 ohbot.reset()
 while True:    
 
-    val1 = ohbot.readSensor(4)
+    val1 = ohbot.readSensor(4)  # Tilt
     
-    val2 = ohbot.readSensor(3)
-    
-    ohbot.setEyeColour(val2,10-val2,0,True)
-    ohbot.move(ohbot.HEADTURN, val2)
-    print(val2)
-    if val1 > 2:
-        ohbot.say("put me down")
-    
-    if val2 < 2:
-        ohbot.say("who turned out the lights")
+    val2 = ohbot.readSensor(3)  # Light
 
-    ohbot.wait(0.1)
+    val3 = ohbot.readSensor(5)  # Touch
+
+    # ohbot.setEyeColour(val2, 10 - val2, 0, True)
+    # ohbot.move(ohbot.HEADTURN, val2)
+    sleep(0.5)
+    print(val3)
+    # if val1 > 2:
+    #     ohbot.say("put me down")
+    #
+    # if val2 < 2:
+    #     ohbot.say("who turned out the lights")
+    #
+    # ohbot.wait(0.1)
 
